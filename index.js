5d52d1c59bae98bc78ec7916b477997c9b29a0fa
Liam Gillespie <liamgillespie1@gmail.com>
2020-04-23 13:01:54 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+for(var i = 0; i < document.querySelectorAll(".drum").length; i ++){
+document.querySelectorAll("button")[i].addEventListener("click", function clicked(){
+  alert("Oi");
+});
+}
